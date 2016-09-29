41e6320b764ae4c08ae79d4b25a27e3d72b2dee2
Audrey-Chen <abq8392@gmail.com>
2016-09-29 22:56:28 +0800

    0930 fix duedate

--- botapp.js
+++ botapp.js
@@ -505,7 +505,7 @@ controller.on('slash_command', function(bot, message) {
                             quali_case = false;
                             break;
                         }
-                        
+
                         if (moment().format('X') + 28800 > team.polling_case[i].details.due_date) {
                             sendVote('overDue');
                             quali_case = false;
@@ -709,25 +709,28 @@ function polling(bot) {
         if (team.hasOwnProperty('polling_case')) {
             var polling_case = team.polling_case;
             for (var i = 0; i < polling_case.length; i++) {
-                if ((polling_case[i].details.due_date - current_time) >= 3600 && (polling_case[i].details.due_date - current_time) < 3660) {
+                if (polling_case[i].details.hasOwnProperty(due_date)) {
+                    if ((polling_case[i].details.due_date - current_time) >= 3600 && (polling_case[i].details.due_date - current_time) < 3660) {
 
-                    var broadcast_im = function broadcast(users_list) {
-                        for (var i = 0; i < users_list.length; i++) {
-                            bot.startPrivateConversation({ user: users_list[i] }, function(err, convo) {
-                                if (err) {
-                                    console.log(err);
-                                } else {
-                                    convo.say(':bell:編號#' + polling_case[i].case_id + '將在一個小時後截止!');
-                                }
-                            });
+                        var broadcast_im = function broadcast(users_list) {
+                            for (var i = 0; i < users_list.length; i++) {
+                                bot.startPrivateConversation({ user: users_list[i] }, function(err, convo) {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        convo.say(':bell:編號#' + polling_case[i].case_id + '將在一個小時後截止!');
+                                    }
+                                });
+                            }
                         }
+
+                        get_list(bot, function(users_list) {
+                            broadcast_im(users_list);
+                        });
+
                     }
-
-                    get_list(bot, function(users_list) {
-                        broadcast_im(users_list);
-                    });
-
                 }
+
             }
         }
     });
